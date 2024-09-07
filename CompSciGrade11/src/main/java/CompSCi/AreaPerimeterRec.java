bc7e0c649a1513d98a3c1a3819f895588527d6b8
Peter Alexander Migliore <47260616+GamerNetwork4@users.noreply.github.com>
2024-09-06 20:39:08 -0400

    Area Perimeter

--- CompSciGrade11/src/main/java/CompSCi/AreaPerimeterRec.java
+++ CompSciGrade11/src/main/java/CompSCi/AreaPerimeterRec.java
@@ -1,4 +1,14 @@
 package CompSCi;
 
 public class AreaPerimeterRec {
+    public static void main(String[] args){
+        int length;
+        int width;
+        length = 25;
+        width = 15;
+
+        System.out.println("The perimeter of the rectangle: " + (2 * length + 2 * width));
+        System.out.println("The area of the rectangle: " + (length * width));
+
+    }
 }
