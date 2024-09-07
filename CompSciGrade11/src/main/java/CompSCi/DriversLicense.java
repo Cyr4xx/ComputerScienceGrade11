c9f9da297d999639807391eb112a9f82db9ba029
Peter Alexander Migliore <47260616+GamerNetwork4@users.noreply.github.com>
2024-09-06 21:21:38 -0400

    Create DriversLicense.java

---
+++ CompSciGrade11/src/main/java/CompSCi/DriversLicense.java
@@ -0,0 +1,24 @@
+package CompSCi;
+
+public class DriversLicense {
+    public static void main(String[] args){
+        String name = "Skibidi"; // Do this next time if lazy
+        String dob;
+        String expiry;
+        String address;
+        String sex;
+
+
+        dob = "June 9, 2003";
+        expiry = "August 18, 2026";
+        address = "777 Jane St.";
+        sex = "Male";
+
+        System.out.println(name + "\n" + dob + "\n" + expiry + "\n" + address + "\n" + sex);
+
+
+
+
+    }
+
+}
