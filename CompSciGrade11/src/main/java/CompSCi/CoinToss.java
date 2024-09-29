9bcbddccc0925050ff52101f163676ae34e752ff
Peter Alexander Migliore <47260616+GamerNetwork4@users.noreply.github.com>
2024-09-29 17:05:53 -0400

    Mathtest/CoinToss

---
+++ CompSciGrade11/src/main/java/CompSCi/CoinToss.java
@@ -0,0 +1,26 @@
+package CompSCi;
+
+import java.util.*;
+
+public class CoinToss {
+    public static void main(String[] args){
+        Random generator = new Random();
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many times would you like to flip a coin?: ");
+        int amount = input.nextInt();
+        int flip = 0;
+        int tail = 0;
+        int head = 0;
+        for (int i = 0; i < amount; i++){
+            flip = generator.nextInt(2)+1;
+            System.out.println(flip);
+            if (flip == 1){
+                head += 1;
+            }
+            else if (flip == 2){
+                tail += 1;
+            }
+        }
+        System.out.println("You flipped heads: " + head + " times and flipped tails " + tail + " times");
+    }
+}
--- CompSciGrade11/src/main/java/CompSCi/MathTest.java
+++ CompSciGrade11/src/main/java/CompSCi/MathTest.java
@@ -9,6 +9,18 @@ public class MathTest {
         System.out.println("Enter first positive value: ");
         int num1 = input.nextInt();
         System.out.println("Enter second positive value: ");
+        int num2 = input.nextInt();
+        if (num1 <= 0 || num2 <= 0){
+            System.out.println("Invalid input.");
+            System.exit(0);
+        }
+        System.out.println("The max between the two numbers is: " + Math.max(num1, num2));
+        System.out.println("The minimum between the two numbers is: " + Math.min(num1, num2));
+        System.out.println("The exponential value is: " + Math.pow(num1, num2));
+        System.out.println("The square root of the first number is: " + Math.sqrt(num1));
+        System.out.println("The square root of the second number is: " + Math.sqrt(num2));
+
+
 
 
     }
