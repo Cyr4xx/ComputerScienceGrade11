f6ef714ee2f9320a00a6d3e83b2c1f6ec02cf106
Peter Alexander Migliore <47260616+GamerNetwork4@users.noreply.github.com>
2024-10-08 22:56:18 -0400

    Update A1Q1.java

--- CompSciGrade11/src/main/java/CompSCi/A1Q1.java
+++ CompSciGrade11/src/main/java/CompSCi/A1Q1.java
@@ -10,20 +10,22 @@ public class A1Q1 {
         System.out.println("Welcome to the Double Dice Rolling Simulator!");
         Scanner input = new Scanner(System.in);
         Random generator = new Random();
-        System.out.println("How many rounds of two-dice would you like to roll? (Please pick a number from 1 - 5): ");
-        int numOfRound = input.nextInt();
+        int numOfRound = 0;
         int sumOfDie = 0;
         String valOfDie = "";
         int dice1 = 0;
         int dice2 = 0;
+        String answer = "y";
 
-        while(true){
+        while(answer.equals("y")){
+            System.out.println("How many rounds of two-dice would you like to roll? (Please pick a number from 1 - 5): ");
+            numOfRound = input.nextInt();
             switch(numOfRound) {
                 case 1:
                     for (int x = 1; x < numOfRound + 1; x++){
                         dice1 = generator.nextInt(6) + 1;
                         dice2 = generator.nextInt(6) + 1;
-                        valOfDie = " Round " + x + ": Dice 1: " + dice1 + " Dice 2: " + dice2;
+                        valOfDie = "Round " + x + "- Dice 1: " + dice1 + " Dice 2: " + dice2;
                         sumOfDie +=  dice1 + dice2;
                         System.out.println(valOfDie);
                     }
@@ -34,7 +36,7 @@ public class A1Q1 {
                     for (int x = 1; x < numOfRound + 1; x++){
                         dice1 = generator.nextInt(6) + 1;
                         dice2 = generator.nextInt(6) + 1;
-                        valOfDie = " Round " + x + ": Dice 1: " + dice1 + " Dice 2: " + dice2;
+                        valOfDie = "Round " + x + "- Dice 1: " + dice1 + " Dice 2: " + dice2;
                         sumOfDie +=  dice1 + dice2;
                         System.out.println(valOfDie);
                     }
@@ -45,7 +47,7 @@ public class A1Q1 {
                     for (int x = 1; x < numOfRound + 1; x++){
                         dice1 = generator.nextInt(6) + 1;
                         dice2 = generator.nextInt(6) + 1;
-                        valOfDie = " Round " + x + ": Dice 1: " + dice1 + " Dice 2: " + dice2;
+                        valOfDie = "Round " + x + "- Dice 1: " + dice1 + " Dice 2: " + dice2;
                         sumOfDie +=  dice1 + dice2;
                         System.out.println(valOfDie);
                     }
@@ -56,7 +58,7 @@ public class A1Q1 {
                     for (int x = 1; x < numOfRound + 1; x++){
                         dice1 = generator.nextInt(6) + 1;
                         dice2 = generator.nextInt(6) + 1;
-                        valOfDie = " Round " + x + ": Dice 1: " + dice1 + " Dice 2: " + dice2;
+                        valOfDie = "Round " + x + "- Dice 1: " + dice1 + " Dice 2: " + dice2;
                         sumOfDie +=  dice1 + dice2;
                         System.out.println(valOfDie);
                     }
@@ -67,7 +69,7 @@ public class A1Q1 {
                     for (int x = 1; x < numOfRound + 1; x++){
                         dice1 = generator.nextInt(6) + 1;
                         dice2 = generator.nextInt(6) + 1;
-                        valOfDie = " Round " + x + ": Dice 1: " + dice1 + " Dice 2: " + dice2;
+                        valOfDie = "Round " + x + "- Dice 1: " + dice1 + " Dice 2: " + dice2;
                         sumOfDie +=  dice1 + dice2;
                         System.out.println(valOfDie);
                     }
@@ -76,13 +78,21 @@ public class A1Q1 {
 
                 default:
                     System.out.println("Invalid number of rounds.");
-                    System.exit(0);
+                    break;
 
             }
-            break;
+
+            System.out.println("Would you like to exit or roll again? (y or n): ");
+
+            answer = input.next();
+            if (answer.equals("n")){// https://stackoverflow.com/questions/8484668/java-does-not-equal-not-working This explains how we can use the "!" does not equal flag for checking strings.
+                System.out.println("Thanks for using the program!");
+
+            } else if (!answer.equals("y") && !answer.equals("n") ) {
+                System.out.println("Invalid Input.");
+                answer = "y";
+
+            }
         }
-
-
-
     }
 }
